d32ffd78f5742006ce51169b952d7f72778f8771
Citlalli Del Moral <citlallidmg@gmail.com>
2025-05-14 18:15:53 -0600

    update: adds mask to phone input

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-console.log("hello worls");
+const phoneInput = document.getElementById("phone");
+
+phoneInput.addEventListener("input", function (e) {
+  let input = e.target.value.replace(/\D/g, ""); // Remove all non-digits
+  if (input.length > 10) input = input.substring(0, 10);
+
+  let formatted = "";
+
+  if (input.length > 0) {
+    formatted += "(" + input.substring(0, 3);
+  }
+  if (input.length >= 4) {
+    formatted += ") " + input.substring(3, 6);
+  }
+  if (input.length >= 7) {
+    formatted += "-" + input.substring(6, 10);
+  }
+
+  e.target.value = formatted;
+});
